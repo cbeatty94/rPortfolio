7acf9191326df29bbc34373eb4cfb118290c9a96
Burke <beatty.burke@gmail.com>
2021-08-22 11:22:53 -0400

    adjusted styling of project images

--- src/data.js
+++ src/data.js
@@ -7,14 +7,14 @@ export const projects = [
     image: "./dawnpatrol.png",
     link: "https://themetalsaint.github.io/TheDawnPatrol/",
   },
-  // {
-  //   title: "React Tracks",
-  //   subtitle: "React and Python",
-  //   description:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-  //   image: "./project-2.gif",
-  //   link: "https://reactbootcamp.com",
-  // },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
   // {
   //   title: "DevChat",
   //   subtitle: "React and Firebase",
