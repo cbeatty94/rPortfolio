d49444d5bd2fdfa9d4fb2711471717f6f3a7fd3c
Burke <beatty.burke@gmail.com>
2021-08-22 10:41:03 -0400

    added encode function to contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,18 @@
 import React from "react";
 
 export default function Contact() {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+
+    function encode(data) {
+        return Object.keys(data)
+            .map(
+                (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+            )
+            .join("&"); 
+    }
+
     return (
         <section id="contact" className="relative">
             <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -94,3 +106,4 @@ export default function Contact() {
         </section>
     )
 }
+
